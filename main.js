39216b0b936106b762c7b9554e931c594eeba686
Erika Zanin <erikaczbu@gmail.com>
2025-04-28 21:35:26 -0300

    Atualizando layout mobile

--- main.js
+++ main.js
@@ -40,19 +40,21 @@ document.querySelectorAll('.btnList').forEach(item => {
     const navBar = document.querySelector('.navbar');
     const content = document.querySelector('.content');
     const isSmallScreen = window.matchMedia('(max-width: 768px)').matches;
+    const body = document.querySelector('body');
+    const html = document.querySelector('html');
 
     if (navBar) {
-       // Aplica a transição no JavaScript
-       if (content) {
-        content.style.transition = 'margin-top 0.5s ease';
-      }
-      if (item) {
-        item.style.transition = 'width 0.5s ease';
-      }
+      // Aplica a transição no JavaScript
+      content?.style.setProperty('transition', 'margin-top 0.5s ease');
+      body?.style.setProperty('transition', 'height 0.5s ease');
+      html?.style.setProperty('transition', 'height 0.5s ease');
+      item?.style.setProperty('transition', 'width 0.5s ease');
 
       if (navBar.style.height && navBar.style.height !== '0px') {
         navBar.style.height = '0px';
         item.style.width = '30%';
+        body.style.height = '100%';
+        html.style.height = '100%';
 
         // Remove a margem se for tela pequena
         if (isSmallScreen && content) {
@@ -60,14 +62,18 @@ document.querySelectorAll('.btnList').forEach(item => {
         }
       } else {
         navBar.style.height = navBar.scrollHeight + 'px';
-        item.style.width = '100%';
 
-        // Adiciona a margem se for tela pequena
-        if (isSmallScreen && content) {
-          content.style.marginTop = '330px';
+        // Ajusta a largura de acordo com o tamanho da tela
+        if (isSmallScreen) {
           item.style.width = '60%';
+          content.style.marginTop = (navBar.scrollHeight + 30) + 'px';
+          body.style.height = '110%';
+          html.style.height = '110%';
+        } else {
+          item.style.width = '100%';
         }
       }
     }
   });
 });
+
